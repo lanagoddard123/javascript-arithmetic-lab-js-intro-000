5e04b298dfc9c24ada5dd57d72b49a0683e94772
lanasgoddard <lanasgoddard@gmail.com>
2018-11-08 13:05:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+var number;
+function add(a,b) {
+  number = a+b
+  return number
+}
+
+function subtract(a,b) {
+  number = a-b
+  return number
+}
+
+function multiply(a,b) {
+  return number = a*b
+}
+
+function divide(a,b) {
+ number = a/b
+ return number
+}
+
+function inc(n) {
+  number = n+1
+  return number
+}
+
+function dec(n) {
+  number = n-1
+  return number
+}
